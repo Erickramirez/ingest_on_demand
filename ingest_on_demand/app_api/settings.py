b6357e9110c57a4f1cde5efc923dd9fc64b528b4
Erick Ramirez <erickramireztebalan@gmail.com>
2022-03-15 23:43:08 -0600

    fix: remove files

--- ingest_on_demand/app_api/settings.py
+++
@@ -1,5 +0,0 @@
-# PG configuration
-pg_db = "postgres"
-pg_host = "127.0.0.1"
-pg_username = "postgres"
-pg_password = "postgres"
--- ingest_on_demand/run_ingest_on_demand.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-python -m ingest_on_demand.cli load-csv
-python -m ingest_on_demand.cli execute-pg-task --event_name=create_schemas
-python -m ingest_on_demand.cli execute-pg-task --event_name=create_tables
-python -m ingest_on_demand.cli execute-pg-task --event_name=update_dimension_tables
-python -m ingest_on_demand.cli execute-pg-task --event_name=update_fact_tables
